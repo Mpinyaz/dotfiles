19443bb91036001eb3e9f082eca83131fb00dce9
Mpinyaz <ebmpinyuri@gmail.com>
2024-08-11 21:18:28 +0200

    which key customization

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -190,20 +190,21 @@ local lazy_plugins = {
 		end,
 	},
 	{ "HiPhish/rainbow-delimiters.nvim" },
-	-- {
-	-- 	"echasnovski/mini.icons",
-	-- 	opts = {},
-	-- 	lazy = true,
-	-- 	specs = {
-	-- 		{ "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
-	-- 	},
-	-- 	init = function()
-	-- 		package.preload["nvim-web-devicons"] = function()
-	-- 			require("mini.icons").mock_nvim_web_devicons()
-	-- 			return package.loaded["nvim-web-devicons"]
-	-- 		end
-	-- 	end,
-	-- },
+	{
+		"echasnovski/mini.icons",
+		opts = {},
+		lazy = true,
+		-- specs = {
+		-- 	{ "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+		-- },
+		init = function()
+			--@diagnostic disable-next-line: duplicate-set-field
+			package.preload["nvim-web-devicons"] = function()
+				require("mini.icons").mock_nvim_web_devicons()
+				return package.loaded["nvim-web-devicons"]
+			end
+		end,
+	},
 	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -5,10 +5,20 @@ return {
 	init = function()
 		local wk = require("which-key")
 		wk.add({
-			{ "<leader>lg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", desc = "Lazygit" },
+			{ "<leader>lg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", desc = "Lazygit", mode = "n" },
 		})
 	end,
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 	opts = {
+		preset = "modern",
 		icons = {
 			breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
 			separator = "➜", -- symbol used between a key and it's label
@@ -18,6 +28,15 @@ return {
 			height = { min = 4, max = 25 }, -- min and max height of the columns
 			width = { min = 20, max = 50 }, -- min and max width of the columns
 			spacing = 3, -- spacing between columns
+		},
+		spec = {
+			{ "<leader>b", group = "Buffers" },
+			{ "<leader>l", group = "LSP" },
+			{ "<leader>g", group = "Git" },
+			{ "<leader>Qf", group = "Quickfix" },
+			{ "<leader>R", group = "Replace" },
+			{ "<leader>s", group = "Search" },
+			{ "<leader>z", group = "Spelling" },
 		},
 		show_help = true,
 	},
